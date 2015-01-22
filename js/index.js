23e751d8db944af06e4f5ef4bce5d78f56247734
Jacob Gronert <jacobgronert@gmail.com>
2015-01-21 21:21:56 -0800

    Implemented set alarm front end
    Created a button to set the alarm time which consists of several inputs
    and a togglable am/pm field.

--- js/index.js
+++ js/index.js
@@ -44,14 +44,33 @@ function returnDoubleDigits(str) {
   return str.toString().length === 1 ? '0' + str : str;
 }
 
+function setAlarm() {
+	$(".alarm").css("opacity", "1");
+	$(".time").css("opacity", "0.4");
+	$(".selector").css("top", "45.5%");
+	$(".selector-text-alarm").css("opacity", "1");
+	$(".selector-text-time").css("opacity", "0");
+	$(".set-alarm-btn").css("display", "none");
+	$(".go-to-sleep-btn").css("display", "initial");
+}
+
+function toggleMeridian() {
+	var meridian = $(".alarm-meridian").text();
+	if(meridian == "am") {
+		$(".alarm-meridian").text("pm");
+	} else {
+		$(".alarm-meridian").text("am");
+	}
+}
+
 function sleep() {
 	$(".go-to-sleep-btn").css("display", "none");
-	$(".wake-up-btn").css("display", "inline");
+	$(".wake-up-btn").css("display", "initial");
 	$("body").css("background-color", "#162145");
 }
 
 function wakeUp() {
-	$(".go-to-sleep-btn").css("display", "inline");
+	$(".go-to-sleep-btn").css("display", "initial");
 	$(".wake-up-btn").css("display", "none");
 	$("body").css("background-color", "#00ABA9");
 }
