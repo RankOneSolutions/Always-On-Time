5bff496a3b9c74cdee82024a2d2c06652ecdebd7
Augustine Wittkower <augustine@retargeter.com>
2015-01-22 17:49:48 -0800

    re-wrote how our js handles time and updates the clock. removed '-btn' suffixes on things that also had the 'btn' class. re-aligned the alarm so it matches the clock. did some general aesthetic changes/pixel pushing. added comments.

--- js/index.js
+++ js/index.js
@@ -1,76 +1,80 @@
-$(document).ready(function (){
-	updateTime();
+//monkeypatch an addMinutes function to Date
+Date.prototype.addMinutes= function(minute) {
+	this.setMinutes(this.getMinutes() + minute);
+	return this;
+}
+
+//this is the random addition to the time, in minutes
+adjustment = 0;
+
+$(function ($) {
+	initTime();
+	initListeners();
+
+	function initTime() {
+		setInterval(function() { clock() }, 500);
+	}
+
+	function initListeners() {
+		$('.sleep').on('click', function () {
+			adjustment = Math.floor(Math.random() * 15) + 1;
+		})
+
+		$('.wake-up').on('click', function () {
+			adjustment = 0;
+		})
+
+		$('.set-alarm').on('click', function() { setAlarm() });
+		$('.wake-up').on('click', function() { wakeUp() });
+		$('.sleep').on('click', function() { sleep() });
+	}
+
+	function clock(){
+		timeSplitter();
+		updateTime();
+	}
+
+	function timeSplitter() {
+		Today  = new Date().addMinutes(adjustment);
+		hour   = enforceDoubleDigits(Today.getHours());
+		minute = enforceDoubleDigits(Today.getMinutes());
+		second = enforceDoubleDigits(Today.getSeconds());
+	}
+
+	function updateTime() {
+		$(".hour").text(hour % 12 + ": ");
+		$(".minute").text(minute  + ": ");
+		$(".second").text(second  + " ");
+	}
+
+  //convert single digits to double digit format, 5 becomes 05.
+  function enforceDoubleDigits(str) {
+  	return str.toString().length === 1 ? '0' + str : str;
+  }
+
+  function setAlarm() {
+  	$(".alarm").css("opacity", "1");
+  	$(".time").css("opacity", "0.4");
+  	$(".selector").css("top", "45.5%");
+  	$(".selector-text-alarm").css("opacity", "1");
+  	$(".selector-text-time").css("opacity", "0");
+  	$(".set-alarm").css("display", "none");
+  	$(".sleep").css("display", "initial");
+  }
+
+  function sleep() {
+  	$(".sleep").css("display", "none");
+  	$(".wake-up").css("display", "initial");
+  	$("body").css("background-color", "#162145");
+  }
+
+  function wakeUp() {
+  	$(".alarm").css("opacity", "0");
+  	$(".set-alarm").css("display", "initial");
+  	$(".sleep").css("display", "none");
+  	$(".wake-up").css("display", "none");
+
+  	$("body").css("background-color", "#00ABA9");
+  }
+
 });
-
-Date.prototype.addMinutes= function(minute){
-    this.setMinutes(this.getMinutes()+minute);
-    return this;
-}
-
-
-rngr = 0;
-
-function updateTime(rngr) {
-	if (rngr){
-		var Today  = new Date().addMinutes(rngr);
-	} else {
-		var Today  = new Date();
-	}
-	var hour   	 = returnDoubleDigits(Today.getHours());
-	var minute 	 = returnDoubleDigits(Today.getMinutes());
-	var second 	 = returnDoubleDigits(Today.getSeconds());
-	var meridian = (hour < 11 ? "am" : "pm");
-
-	$(".hour").text(hour % 12 + ": ");
-	$(".minute").text(minute + ": ");
-
-	if (second.toString().length == 1) {
-		second = "0" + second;
-	} else {
-		$(".second").text(second + " " + meridian);
-	}
-
-	$('.go-to-sleep-btn').on('click', function(){
-		rngr = Math.floor(Math.random() * 15) + 1;
-	})
-
-	$('.wake-up-btn').on('click', function(){
-		rngr = 0;
-	})
-		setTimeout(function(){updateTime(rngr)},500);
-}
-
-function returnDoubleDigits(str) {
-  return str.toString().length === 1 ? '0' + str : str;
-}
-
-function setAlarm() {
-	$(".alarm").css("opacity", "1");
-	$(".time").css("opacity", "0.4");
-	$(".selector").css("top", "45.5%");
-	$(".selector-text-alarm").css("opacity", "1");
-	$(".selector-text-time").css("opacity", "0");
-	$(".set-alarm-btn").css("display", "none");
-	$(".go-to-sleep-btn").css("display", "initial");
-}
-
-function toggleMeridian() {
-	var meridian = $(".alarm-meridian").text();
-	if(meridian == "am") {
-		$(".alarm-meridian").text("pm");
-	} else {
-		$(".alarm-meridian").text("am");
-	}
-}
-
-function sleep() {
-	$(".go-to-sleep-btn").css("display", "none");
-	$(".wake-up-btn").css("display", "initial");
-	$("body").css("background-color", "#162145");
-}
-
-function wakeUp() {
-	$(".go-to-sleep-btn").css("display", "initial");
-	$(".wake-up-btn").css("display", "none");
-	$("body").css("background-color", "#00ABA9");
-}
